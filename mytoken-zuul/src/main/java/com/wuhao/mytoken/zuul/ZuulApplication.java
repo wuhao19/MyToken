3791a21bea037f5649a8a54bff8cb3ddc10c900d
Administrator <928707094@qq.com>
2018-12-20 16:58:14 +0800

    init 更新所有生产环境配置文件

--- mytoken-zuul/src/main/java/com/wuhao/mytoken/zuul/ZuulApplication.java
+++ mytoken-zuul/src/main/java/com/wuhao/mytoken/zuul/ZuulApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableZuulProxy
-@EnableEurekaClient
 @SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
 public class ZuulApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZuulApplication.class,args);
